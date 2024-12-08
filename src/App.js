ec377f8e18c9fdd2f9a7f03551acc6046fd5c5a0
rohit <davasrohit@gmail.com>
2024-12-08 02:19:10 +0100

    added google analytics addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import useTheme from './hooks/useTheme';
 import GlobalStyles from './styles/GlobalStyles';
@@ -9,11 +10,31 @@ import Timeline from './pages/Timeline';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
+// Wrapper component to track page views
+const RouteTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview', 
+      page: location.pathname
+    });
+  }, [location]);
+
+  return null;
+};
+
 function App() {
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize('G-S0WE9752CD');
+  }, []);
+
   const { theme, toggleTheme } = useTheme();
 
   return (
     <Router>
+      <RouteTracker /> {/* Add route tracking */}
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Navbar toggleTheme={toggleTheme} />
